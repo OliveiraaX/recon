f859e3bcc3a35bb3a6bdd61370701965aedc8777
OliveiraX <131061822+OliveiraaX@users.noreply.github.com>
2023-11-22 18:22:23 -0300

    recon.sh

--- recon.sh
+++ recon.sh
@@ -1,67 +1,64 @@
 #!/bin/bash
-# Script create by OliveiraX
+# Script created by OliveiraX
 # GitHub: https://github.com/OliveiraaX
+
 if [ "$#" -lt 2 ] || [ "$#" -gt 4 ]; then
-    echo "Uso: $0 <alvo> <user-agent> [wordlist] [extensao]"
+    echo "Usage: $0 <target> <user-agent> [wordlist] [extension]"
     exit 1
 fi
 
-read -p "---- Deseja executar varredura dns(x) ou dominio(z)?(x/z): " choice
+read -p "---- Do you want to perform a DNS scan (x) or a domain scan (z)? (x/z): " choice
 case "$choice" in
     z)
         echo "=================================================================="
-        echo "                 [+] Executando varredura"
+        echo "                 [+] Performing scan"
         echo "=================================================================="
 
-        servico=$(curl -s -H "User-Agent: $2" "$1/auwn#!" | grep -oP '<address>\K[^<]*' | grep -oP '^[^ ]+')
+        service=$(curl -s -H "User-Agent: $2" "$1/auwn#!" | grep -oP '<address>\K[^<]*' | grep -oP '^[^ ]+')
         echo ""
-        echo " - WebServer $servico"
+        echo " - WebServer $service"
         echo ""
         echo "--------------------------- Scanning ----------------------------"
-        for palavra in $(cat "${3:-/dev/stdin}"); do
-            url="$1/$palavra"
-            url_com_barra="$url/"
+        for word in $(cat "${3:-/dev/stdin}"); do
+            url="$1/$word"
+            url_with_slash="$url/"
 
-            # Verifica se a URL sem barra existe
-            saida=$(curl -s -H "User-Agent: $2" -o /dev/null -w "%{http_code}" "$url${4:+$4}")
-            if [ "$saida" == "200" ]; then
+            # Check if the URL without a slash exists
+            output=$(curl -s -H "User-Agent: $2" -o /dev/null -w "%{http_code}" "$url${4:+$4}")
+            if [ "$output" == "200" ]; then
                 if [ -n "$4" ]; then
-                    echo "Arquivo encontrado: $url$4"
+                    echo "File found: $url$4"
                 else
-                    echo "Arquivo encontrado: $url"
+                    echo "File found: $url"
                 fi
-
             fi
 
-            # Verifica se a URL com barra existe
-            saida=$(curl -s -H "User-Agent: $2" -o /dev/null -w "%{http_code}" "$url_com_barra")
-            if [ "$saida" == "200" ]; then
-                echo "Diretório encontrado: $url_com_barra"
+            # Check if the URL with a slash exists
+            output=$(curl -s -H "User-Agent: $2" -o /dev/null -w "%{http_code}" "$url_with_slash")
+            if [ "$output" == "200" ]; then
+                echo "Directory found: $url_with_slash"
             fi
-
         done
-           echo "-----------------------------------------------------------------"
+        echo "-----------------------------------------------------------------"
         ;;
 
     x)
         echo "=================================================================="
-        echo "                 [+] Executando varredura"
+        echo "                 [+] Performing scan"
         echo "=================================================================="
-        servico1=$(curl -s -H "User-Agent: $2" "$1/auwn#!" | grep -oP '<address>\K[^<]*' | grep -oP '^[^ ]+')
+        service1=$(curl -s -H "User-Agent: $2" "$1/auwn#!" | grep -oP '<address>\K[^<]*' | grep -oP '^[^ ]+')
         echo ""
-        echo " - WebServer $servico1"
+        echo " - WebServer $service1"
         echo ""
         echo "--------------------------- Scanning ----------------------------"
-        for i in $(cat "$3");do
+        for i in $(cat "$3"); do
             host $i.$1 | grep "address"
         done
-        
         echo "-----------------------------------------------------------------"
 
         ;;
     *)
-        echo "Escolha inválida. Saindo."
+        echo "Invalid choice. Exiting."
         exit 1
         ;;
 esac
-
